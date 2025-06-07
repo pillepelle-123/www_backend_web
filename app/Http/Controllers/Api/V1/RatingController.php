6c68fac2f51d58fdb42a7948908b31c63d87453f
Peter S <pillepelle@freenet.de>
2025-06-07 08:09:32 +0200

    RatingController mit ausgeklügelter Filter Logik (manueller Eigenbau)

--- app/Http/Controllers/Api/V1/RatingController.php
+++ app/Http/Controllers/Api/V1/RatingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Api\ApiController;
 use App\Models\Rating;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class RatingController extends ApiController
 {
@@ -13,17 +14,88 @@ class RatingController extends ApiController
      */
     public function index(Request $request)
     {
+        $fields = [
+            'ratings.id', 'ratings.score', 'ratings.comment', 'ratings.created_at', 'ratings.updated_at', 'offers.id', 'offers.user_id', 'offers.offer_title', 'offers.reward_total_cents', 'offers.reward_offerer_percent', 'offers.status', 'users_from.name', 'users_to.name'
+        ];
+
+        $fieldsWithAliases = array_map(
+            function ($field) {
+                // Sonderfall: Entferne "ratings." im Alias, falls vorhanden
+                $alias = str_replace('.', '-', $field);
+                if (str_starts_with($field, 'ratings.')) {
+                    $alias = str_replace('ratings-', '', $alias);
+                }
+                return $field . ' as ' . $alias;
+            },
+            $fields
+        );
+
         $query = Rating::query()
             ->join('offers', 'ratings.offer_id', '=', 'offers.id')
             ->join('users as users_from', 'ratings.from_user_id', '=', 'users_from.id')
             ->join('users as users_to', 'ratings.to_user_id', '=', 'users_to.id')
-            // ->join('companies', 'offers.company_id', '=', 'companies.id')
-            ->select('ratings.id', 'offers.id as offers-id', 'offers.user_id as offers-user_id', 'offers.offer_title as offers-offer_title', 'offers.reward_total_cents as offers-reward_total_cents', 'offers.reward_offerer_percent as offers-reward_offerer_percent', 'offers.status as offers-status', 'users_from.name as users_from-name', 'users_to.name as users_to-name')
+            ->select($fieldsWithAliases)
         ;
 
-        $columns = array_keys($query->first()->toArray());
+        // nur Parameter mit filter[], search und sort_by erlaubt
+        $request->only(['filter', 'search', 'sort_by']);
+        // only Request Filter Parameter mit Feldern aus $fields erlaubt
+        $query_filter_fields = $request->query('filter');
+
+        $allowed_fields = [];
+        foreach($query_filter_fields as $key => $value) {
+            if(in_array($key, str_replace('.', '-', $fields))) {
+                $allowed_fields[$key] = $value;
+            }
+        }
+
+        foreach ($allowed_fields as $key => $value) {
+            foreach ($request->input('filter.' . $key, []) as $op => $value) {
+
+            $key = str_replace('-', '.', $key);
+
+                match ($op) {
+                    'gt'  => $query->where($key, '>', $value),
+                    'gte' => $query->where($key, '>=', $value),
+                    'lt'  => $query->where($key, '<', $value),
+                    'lte' => $query->where($key, '<=', $value),
+                    default => $query->where($key, '=', $value),
+                };
+            }
+        }
+        // die($query->toSql());
+
+
+        // $fields = [
+        //     'ratings.*', 'offers.id as offers-id', 'offers.user_id as offers-user_id', 'offers.offer_title as offers-offer_title', 'offers.reward_total_cents as offers-reward_total_cents', 'offers.reward_offerer_percent as offers-reward_offerer_percent', 'offers.status as offers-status', 'users_from.name as users_from-name', 'users_to.name as users_to-name'
+        // ];
+
+
+
+        // $fields = ['filter[offers-offer_title][]'];
+
+        // nur Parameter mit filter[] erlaubt
+        // $filters = $request->only(['filter']); //$fields);
+        // // foreach ($filters as $field => $value) {
+        // //     if ($request->has('filter.' . $field)) {
+        // //         $query->whereIn('category_id', $request->input('filter.' . $field));
+        // //     }
+        // // }
+
+        // $filters = $request->query('filter');
+
+        // ;
+
+        // only Request Filter Parameter mit Feldern aus $fields erlaubt
+        // dd(array_intersect($fields, array_keys($filters)));
+
+        // if(array_keys($filters) == $fields) {
+
+        // }
+
 
         // Search Parameter
+        $columns = array_keys($query->first()->toArray());
         [$key, $value] = $this->getFirstSearchParam($request->query->all());
         if($key && $value) {
             if(array_search($key, $columns))
@@ -34,14 +106,14 @@ class RatingController extends ApiController
         }
 
         // Filter-Parameter
-        $filters = $request->only(['offers-id', 'offers-user_id', 'offers-offered_by_type', 'offers-offer_title', 'offers-reward_total_cents', 'created_at', 'updated_at', 'offers-status', 'users_from-name', 'users_to-name']);
-        foreach ($filters as $field => $value) {
-            // Felder aus fremden Tabellen werden mit "tabelle-feld_name" angesprochen. Hier wird - (Bindestrich) als Trenner genommen und im folgenden durch . ersetzt, da . (Punkt) im URL-Paramter nicht vorkommen darf.
-            if (str_contains($field, '-')) {
-                $field = str_replace('-', '.', $field);
-            };
-            $query->where($field, $value);
-        }
+        // $filters = $request->only($fields);
+        // foreach ($filters as $field => $value) {
+        //     // Felder aus fremden Tabellen werden mit "tabelle-feld_name" angesprochen. Hier wird - (Bindestrich) als Trenner genommen und im folgenden durch . ersetzt, da . (Punkt) im URL-Paramter nicht vorkommen darf.
+        //     if (str_contains($field, '-')) {
+        //         $field = str_replace('-', '.', $field);
+        //     };
+        //     $query->where($field, '=', $value);
+        // }
 
         // Sort-Parameter
         if ($request->has('sort_by')) {
--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -15,31 +15,100 @@ class UserController extends ApiController
      */
     public function index(Request $request)
     {
-        // Query Aufbau
-        $query = User::query();
 
-        // Search Parameter
-        [$key, $value] = $this->getFirstSearchParam($request->query->all());
-        if($key && $value) {
-            $query->where($key, 'LIKE', '%' . $value . '%');
-        }
+        $users = User::all(); // Oder paginieren
+        $userResource = UserResource::collection($users);
 
+        dd($userResource);
 
-        // Filter-Parameter
-        $filters = $request->only(['name', 'email', 'created_at', 'updated_at']);
-        foreach ($filters as $field => $value) {
-            // Felder aus fremden Tabellen werden mit "tabelle-feld_name" angesprochen. Hier wird - (Bindestrich) als Trenner genommen und im folgenden durch . ersetzt, da . (Punkt) im URL-Paramter nicht vorkommen darf.
-            if (str_contains($field, '-')) {
-                $field = str_replace('-', '.', $field);
+        $fields = [
+            'users-name', 'users-email', 'users-email_verified_at', 'users-created_at', 'users-updated_at'
+        ];
+
+        $query = User::query()
+            ->join('offers', 'ratings.offer_id', '=', 'offers.id')
+            // ->join('users as users_from', 'ratings.from_user_id', '=', 'users_from.id')
+            // ->join('users as users_to', 'ratings.to_user_id', '=', 'users_to.id')
+            // ->join('companies', 'offers.company_id', '=', 'companies.id')
+            ->select(str_replace('-', '.', $fields))
+        ;
+
+        // nur Parameter mit filter[], search und sort_by erlaubt
+        $request->only(['filter', 'search', 'sort_by']);
+
+        // only Request Filter Parameter mit Feldern aus $fields erlaubt
+        $filter_fields = $request->query('filter');
+        $allowed_fields = array_intersect($fields, array_keys($filter_fields));
+
+        foreach ($allowed_fields as $allowed_field) {
+            foreach ($request->input('filter.' . $allowed_field, []) as $op => $value) {
+
+            if (str_contains($allowed_field, '-')) {
+                $allowed_field = str_replace('-', '.', $allowed_field);
             };
-            $query->where($field, $value);
+
+                // dump('allowed_filter: ' . $allowed_field . ', op: ' . $op . ', val: ' . $value);
+                match ($op) {
+                    'gt'  => $query->where($allowed_field, '>', $value),
+                    'gte' => $query->where($allowed_field, '>=', $value),
+                    'lt'  => $query->where($allowed_field, '<', $value),
+                    'lte' => $query->where($allowed_field, '<=', $value),
+                    default => $query->where($allowed_field, '=', $value),
+                };
+            }
         }
 
-        // Sort-Parameter
-        if ($request->has('sort_by')) {
-            $sortOrder = $request->input('sort_order', 'asc');
-            $query->orderBy($request->input('sort_by'), $sortOrder);
-        }
+
+
+        // $fields = [
+        //     'name', 'email', 'created_at', 'updated_at'
+        // ];
+
+        // // Query Aufbau
+        // $query = User::query()
+        //     ->select($fields);
+
+        // $filters = $request->only($fields);
+        // // foreach ($filters as $field => $value) {
+        // //     if ($request->has('filter.' . $field)) {
+        // //         $query->whereIn('category_id', $request->input('filter.' . $field));
+        // //     }
+        // // }
+
+        // foreach ($request->input('filter.', []) as $op => $value) {
+        //     foreach ($filters as $field => $value) {
+        //         match ($op) {
+        //             'gt'  => $query->where($field, '>', $value),
+        //             'gte' => $query->where($field, '>=', $value),
+        //             'lt'  => $query->where($field, '<', $value),
+        //             'lte' => $query->where($field, '<=', $value),
+        //             default => null
+        //         };
+        //     }
+        // }
+
+        // // Search Parameter
+        // [$key, $value] = $this->getFirstSearchParam($request->query->all());
+        // if($key && $value) {
+        //     $query->where($key, 'LIKE', '%' . $value . '%');
+        // }
+
+
+        // // Filter-Parameter
+        // $filters = $request->only(['name', 'email', 'created_at', 'updated_at']);
+        // foreach ($filters as $field => $value) {
+        //     // Felder aus fremden Tabellen werden mit "tabelle-feld_name" angesprochen. Hier wird - (Bindestrich) als Trenner genommen und im folgenden durch . ersetzt, da . (Punkt) im URL-Paramter nicht vorkommen darf.
+        //     if (str_contains($field, '-')) {
+        //         $field = str_replace('-', '.', $field);
+        //     };
+        //     $query->where($field, $value);
+        // }
+
+        // // Sort-Parameter
+        // if ($request->has('sort_by')) {
+        //     $sortOrder = $request->input('sort_order', 'asc');
+        //     $query->orderBy($request->input('sort_by'), $sortOrder);
+        // }
 
         $perPage = $request->input('per_page', 10);
         $paginatedItems = $query->paginate($perPage);
