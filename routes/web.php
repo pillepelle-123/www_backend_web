4e81c107e44ae7322ed9b913a52af4d6463c2354
Peter S <pillepelle@freenet.de>
2025-06-21 12:32:18 +0200

    profile.edit doppelt in Routen

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,6 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     // Offers
     Route::prefix('offers')->name('web.offers.')->group(function () {
         Route::get('/', [OfferController::class, 'index'])->name('index');
